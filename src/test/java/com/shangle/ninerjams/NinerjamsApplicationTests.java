f898ae59346a31ef4857fe7f3bd76c69401e46ad
mshangle1 <mshangle207@gmail.com>
2020-05-26 20:41:03 -0400

    Initial commit

---
+++ src/test/java/com/shangle/ninerjams/NinerjamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shangle.ninerjams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinerjamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
